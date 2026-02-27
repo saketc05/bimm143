d41459a45043cebd85125438f3d1e15ef7277244
Saket Chodavarapu <saketchodavarapu@Sakets-MacBook-Air.local>
2026-02-27 10:57:03 -0800

    Uploading all previous classes' work to Github

---
+++ Class4Jan16/Class4.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Saket Chodavarapu"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x), type="l", col="#137F53", lwd=3,
+     xlab="Is it lunch time yet?")
